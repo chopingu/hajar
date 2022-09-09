4683c4d506b9eea1d6d049a5f752635c6517ffa6
JonathanHallstrom <lmj.hallstrom@gmail.com>
2022-09-09 17:18:54 +0200

    simplify and fix more bugs in neural_net code

--- src/neural_net_testing/neural_net.hpp
+++ src/neural_net_testing/neural_net.hpp
@@ -7,7 +7,7 @@
 namespace gya {
     template<class T, class F1, class F2, u64... sizes>
     struct neural_net {
-        layer_array<T, sizes...> values;
+        layer_array<T, sizes...> m_values;
         layer_array<T, sizes...> m_biases;
         weight_array<T, sizes...> m_weights;
         F1 activation_function;
@@ -25,7 +25,7 @@ namespace gya {
         }
 
         layer_array<T, sizes...> calculate_deltas(std::span<T> correct_output) const {
-            std::span<T> input{values.front()}, output{values.back()};
+            std::span<T> input{m_values.front()}, output{m_values.back()};
             layer_array < T, sizes...> deltas;
             for (u64 i = 0; i < output.size(); ++i) {
                 deltas[i] = output[i] - correct_output[i];
@@ -37,18 +37,19 @@ namespace gya {
 
         auto back_propagate(std::span<T> correct_output) {
             const auto deltas = calculate_deltas(correct_output);
-            std::span<T> input{values.front()}, output{values.back()};
+            std::span<T> input{m_values.front()}, output{m_values.back()};
 
         }
 
-        [[nodiscard]] auto evaluate(std::span<T> inp) {
+        [[nodiscard]] auto evaluate_impl(std::span<T> inp, layer_array<T, sizes...>& values) const {
             std::span<T> input{values.front()}, output{values.back()};
             std::copy(inp.begin(), inp.end(), input.begin());
 
             for (u64 i = 0; i + 1 < values.size(); ++i) {
                 for (u64 j = 0; j < values[i].size(); ++j) {
+                    values[i][j] = activation_function(values[i][j]);
                     for (u64 k = 0; k < values[i + 1].size(); ++k) {
-                        values[i + 1][k] += activation_function(values[i][j] * m_weights[i][j][k] + m_biases[i + 1][k]);
+                        values[i + 1][k] += values[i][j] * m_weights[i][j][k] + m_biases[i + 1][k];
                     }
                 }
             }
@@ -56,19 +57,13 @@ namespace gya {
             return output;
         }
 
+        [[nodiscard]] auto evaluate(std::span<T> inp) {
+            return evaluate_impl(inp, m_values);
+        }
+
         [[nodiscard]] auto evaluate_const(std::span<T> inp) const {
             layer_array < T, sizes...> values;
-            std::span<T> input{values.front()}, output{values.back()};
-            std::copy(inp.begin(), inp.end(), input.begin());
-
-            for (u64 i = 0; i + 1 < values.size(); ++i) {
-                for (u64 j = 0; j < values[i].size(); ++j) {
-                    for (u64 k = 0; k < values[i + 1].size(); ++k) {
-                        values[i + 1][k] += activation_function(values[i][j] * m_weights[i][j][k] + m_biases[i + 1][k]);
-                    }
-                }
-            }
-
+            auto output = evaluate_impl(values);
             return std::array(output.begin(), output.end());
         }
 

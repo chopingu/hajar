33a7634663e98c9e09ae94654501955b19119f61
Adam Amanbaev <adam.amanbaev@gmail.com>
2022-09-24 22:53:20 +0200

    added missed namespaced

--- src/cnn/error_f.hpp
+++ src/cnn/error_f.hpp
@@ -11,25 +11,25 @@ namespace cnn
 
 namespace mse 
 {
-string name="mse";
+std::string name="mse";
 f32 error(f32 output, f32 target) { return 0.5f * (output - target) * (output - target); }
 f32 pd_error(f32 output, f32 target) { return out - target; }
 }
 
 namespace bce
 {
-string name="bce";
+std::string name="bce";
 f32 error(f32 output, f32 target) { return (-target * std::log(output) - (1.0f - target) * std::log(1.0f - output)); }
 f32 pd_error(f32 output, f32 target) { return (output - target) / (output * (1.0f - output)); }
 }
 
 struct error_function {
-    string *name;
+    std::string *name;
     f32 (*error)(f32, f32);
     f32 (*pd_error)(f32, f32);
 }
 
-error_function* new_error_function(string new_function) {
+error_function* new_error_function(std::string new_function) {
     error_function *nw = new error_function;
 
     if(new_function==mse::name) { 

10f139021b0cb40c5db1853baf9be55acc71ba09
JonathanHallstrom <lmj.hallstrom@gmail.com>
2022-10-07 09:15:13 +0200

    fix two move solver

--- src/heuristic/two_move_solver.hpp
+++ src/heuristic/two_move_solver.hpp
@@ -8,32 +8,32 @@ struct two_move_solver {
     gya::random_player rp{};
 
     u8 operator()(gya::board const &b) {
-        i8 turn = b.size % 2 ? 1 : -1;
+        i8 turn = b.turn();
+        for (u8 i = 0; i < 7; ++i) {
+            gya::board copy = b;
+            if (copy.data[i].height == 6)
+                continue;
+            copy.play(i);
+            if ((turn == 1 && copy.has_won().player_1_won()) || (turn == -1 && copy.has_won().player_2_won()))
+                return i;
+        }
+        turn = -turn;
         for (u8 i = 0; i < 7; ++i) {
             gya::board copy = b;
             if (copy.data[i].height == 6)
                 continue;
             copy.play(i, turn);
-            if (copy.has_won().state == turn)
+            if ((turn == 1 && copy.has_won().player_1_won()) || (turn == -1 && copy.has_won().player_2_won()))
                 return i;
         }
-        turn = turn == 1 ? -1 : 1;
-        for (u8 i = 0; i < 7; ++i) {
-            gya::board copy = b;
-            if (copy.data[i].height == 6)
-                continue;
-            copy.play(i, turn);
-            if (copy.has_won().state == turn)
-                return i;
-        }
-        turn = turn == 1 ? -1 : 1;
+        turn = -turn;
         for (int i = 0; i < 16; ++i) {
             auto move = rp(b);
             gya::board copy = b;
             copy.play(move, turn);
             if (copy.data[move].height < 6)
-                copy.play(move, turn == 1 ? -1 : 1);
-            if (copy.has_won_test().state != (turn == 1 ? -1 : 1))
+                copy.play(move, -turn);
+            if ((turn == -1 && copy.has_won().player_1_won()) || (turn == 1 && copy.has_won().player_2_won()))
                 return move;
         }
         return rp(b);

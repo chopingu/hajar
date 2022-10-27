86353308635367f0a775901a247a4cb6c31046a8
Adam Amanbaev <adam.amanbaev@gmail.com>
2022-10-27 09:14:17 +0200

    added tensor class

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "heuristic/two_move_solver.hpp"
 #include "heuristic/n_move_solver.hpp"
 #include "heuristic/n_move_solver_simple.hpp"
+#include "pinguml/tensor.hpp"
 
 #include <thread>
 #include <omp.h>

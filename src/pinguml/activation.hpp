9c4649508203ebc18aaaf614b3ab5ea2f319fa5e
Adam Amanbaev <adam.amanbaev@gmail.com>
2022-11-23 21:48:51 +0100

    fixed typos

--- src/pinguml/activation.hpp
+++ src/pinguml/activation.hpp
@@ -6,42 +6,52 @@
 
 namespace pinguml {
 
-// ----- identity ----- //
+// ----- null ----- //
 
-auto identity = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) 
-        input[i] = input[i] + bias[i];
+auto null = [](f32 *input, const f32 *biases, const u32 size) {
+    return;
 };
 
-auto identity_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) 
+auto null_c = [](f32 *input, const f32 bias, const u32 size) {
+    return;
+};
+
+// ----- identity ----- //
+
+auto identity = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) 
+        input[i] = input[i] + biases[i];
+};
+
+auto identity_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) 
         input[i] = input[i] + bias;
 };
 
 // ----- tanh ------ //
 
-auto tanh = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) 
-        input[i] = std::tanh(input[i] + bias[i]);
+auto tanh = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) 
+        input[i] = std::tanh(input[i] + biases[i]);
 };
 
-auto tanh_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) 
+auto tanh_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) 
         input[i] = std::tanh(input[i] + bias);
 };
 
 // ----- elu ----- //
 
-auto elu = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
-        const f32 x = input[i] + bias[i];
+auto elu = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
+        const f32 x = input[i] + biases[i];
         if(x < 0.f) input[i] = 0.1f * (std::exp(x) - 1.f);
         else input[i] = x;
     }
 };
 
-auto elu_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
+auto elu_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
         const f32 x = input[i] + bias;
         if(x < 0.f) input[i] = 0.1f * (std::exp(x) - 1.f);
         else input[i] = x;
@@ -50,16 +60,16 @@ auto elu_c = [](f32 *input, const f32 bias, const u32 _size) {
 
 // ----- relu ----- //
 
-auto relu = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
-        const f32 x = input[i] + bias[i];
+auto relu = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
+        const f32 x = input[i] + biases[i];
         if(x < 0.f) input[i] = 0.f;
         else input[i] = x;
     }
 };
 
-auto relu_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
+auto relu_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
         const f32 x = input[i] + bias;
         if(x < 0.f) input[i] = 0.f;
         else input[i] = x;
@@ -68,16 +78,16 @@ auto relu_c = [](f32 *input, const f32 bias, const u32 _size) {
 
 // ----- lrelu ----- //
 
-auto lrelu = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
-        const f32 x = input[i] + bias[i];
+auto lrelu = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
+        const f32 x = input[i] + biases[i];
         if(x < 0.f) input[i] = 0.01f * x;
         else input[i] = x;
     }
 };
 
-auto lrelu_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
+auto lrelu_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
         const f32 x = input[i] + bias;
         if(x < 0.f) input[i] = 0.01f * x;
         else input[i] = x;
@@ -86,16 +96,16 @@ auto lrelu_c = [](f32 *input, const f32 bias, const u32 _size) {
 
 // ----- vlrelu ----- //
 
-auto vlrelu = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
-        const f32 x = input[i] + bias[i];
+auto vlrelu = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
+        const f32 x = input[i] + biases[i];
         if(x < 0.f) input[i] = 0.3f * x;
         else input[i] = x;
     }
 };
 
-auto vlrelu_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) {
+auto vlrelu_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) {
         const f32 x = input[i] + bias;
         if(x < 0.f) input[i] = 0.3f * x;
         else input[i] = x;
@@ -104,13 +114,13 @@ auto vlrelu_c = [](f32 *input, const f32 bias, const u32 _size) {
 
 // ----- sigmoid ----- //
 
-auto sigmoid = [](f32 *input, const f32 *bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) 
-        input[i] = 1.0f / (1.0f + std::exp(-(input[i] + bias[i])));
+auto sigmoid = [](f32 *input, const f32 *biases, const u32 size) {
+    for(u32 i = 0; i < size; i++) 
+        input[i] = 1.0f / (1.0f + std::exp(-(input[i] + biases[i])));
 };
 
-auto sigmoid_c = [](f32 *input, const f32 bias, const u32 _size) {
-    for(u32 i = 0; i < _size; i++) 
+auto sigmoid_c = [](f32 *input, const f32 bias, const u32 size) {
+    for(u32 i = 0; i < size; i++) 
         input[i] = 1.0f / (1.0f + std::exp(-(input[i] + bias)));
 };
 

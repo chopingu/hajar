8d82c5bab56777a69afa6d1df78b9e7358c1aa04
Adam Amanbaev <adam.amanbaev@gmail.com>
2022-12-10 15:58:02 +0100

    added cost and math in utils

---
+++ src/pinguml/utils/cost.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../../include.hpp"
+
+namespace pinguml {
+
+// ----- MSE ----- //
+
+auto mse = [](const f32 output, const f32 target) {
+    return 0.5f * (output - target) * (output - target);
+};
+
+auto d_mse = [](const f32 output, const f32 target) {
+    return output - target;
+};
+
+// ----- CROSS ENTROPY ----- //
+
+auto cross_entropy = [](const f32 output, const f32 target) {
+    return -target * std::log(output) - (1.f - target) * std::log(1.f - output);
+};
+
+auto d_cross_entropy = [](const f32 output, const f32 target) {
+    return (output - target) / (output * (1.f - output));
+};
+
+} // namespace pinguml
---
+++ src/pinguml/utils/math.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../include.hpp"
+
+namespace pinguml {
+
+f32 dot(const f32 *a, const f32 *b, const u32 size) {
+    f32 sum = 0;
+    for (u32 i = 0; i < size; i++)
+        sum += a[i] * b[i];
+
+    return sum;
+}
+
+} // pinguml

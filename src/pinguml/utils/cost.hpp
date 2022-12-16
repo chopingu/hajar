bfca79d367b5cb2221b2f6ae81bdc0823eb51834
JonathanHallstrom <lmj.hallstrom@gmail.com>
2022-12-16 10:46:19 +0100

    improve performance of cost functions in cost.hpp

--- src/pinguml/utils/cost.hpp
+++ src/pinguml/utils/cost.hpp
@@ -14,11 +14,11 @@ public:
 
 class mse : public cost_base {
 public:
-    virtual f32 cost([[maybe_unused]] const f32 output, [[maybe_unused]] const f32 target) {
+    virtual f32 cost(const f32 output, const f32 target) final override {
         return 0.5f * (output - target) * (output - target);
     }
 
-    virtual f32 cost_d([[maybe_unused]] const f32 output, [[maybe_unused]] const f32 target) {
+    virtual f32 cost_d(const f32 output, const f32 target) final override {
         return output - target;
     }
 };
@@ -26,21 +26,22 @@ public:
 // ----- CROSS ENTROPY ----- //
 
 class cross_entropy : public cost_base {
-    virtual f32 cost([[maybe_unused]] const f32 output, [[maybe_unused]] const f32 target) {
+    virtual f32 cost(const f32 output, const f32 target) final override {
         return -target * std::log(output) - (1.f - target) * std::log(1.f - output);
     }
 
-    virtual f32 cost_d([[maybe_unused]] const f32 output, [[maybe_unused]] const f32 target) {
+    virtual f32 cost_d(const f32 output, const f32 target) final override {
         return (output - target) / (output * (1.f - output));
     }
 };
 
-cost_base *create_cost(const std::string type) {
-    if(type == "mse") return new mse();
-    else if (type == "cross_entropy") return new cross_entropy();
-    else throw std::runtime_error("invalid cost-function '" + type + "'");
-
-    return nullptr;
+cost_base *create_cost(std::string_view type) {
+    if (type == "mse")
+        return new mse();
+    else if (type == "cross_entropy")
+        return new cross_entropy();
+    else
+        throw std::runtime_error("invalid cost-function '" + std::string{type} + "'");
 }
 
 } // namespace pinguml

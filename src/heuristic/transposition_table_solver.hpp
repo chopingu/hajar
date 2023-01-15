d12137bdc9ebc9741d38759ec03dbf378fbadb3a
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-01-15 18:28:00 +0100

    minor tweaks

--- src/heuristic/transposition_table_solver.hpp
+++ src/heuristic/transposition_table_solver.hpp
@@ -6,8 +6,7 @@
 namespace heuristic {
 struct transposition_table_solver {
     constexpr static usize hash_bytes(std::span<const char> bytes) {
-        usize result =
-                sizeof(usize) == 8 ? 14695981039346656037ULL : 2166136261U;
+        usize result = sizeof(usize) == 8 ? 14695981039346656037ULL : 2166136261U;
 
         for (auto i: bytes) {
             result ^= i;
@@ -18,28 +17,24 @@ struct transposition_table_solver {
 
     struct compressed_board_hasher {
         constexpr usize operator()(gya::compressed_board const &b) const noexcept {
-            return hash_bytes(std::span(std::bit_cast<const char *>(&b), sizeof(b)));
+            auto sp = std::span<const char>(std::bit_cast<const char *>(&b), sizeof(b));
+            return hash_bytes(sp);
         };
     };
 
-    constexpr static compressed_board_hasher hasher{};
+    // constexpr static compressed_board_hasher hasher{};
 
     i32 m_depth = 5;
-    lmj::hash_table<gya::compressed_board, heuristic::eval_result,
-            compressed_board_hasher>
-            m_ttable{};
+    lmj::hash_table<gya::compressed_board, heuristic::eval_result, compressed_board_hasher> m_ttable{};
 
-    [[nodiscard]] eval_result evaluate_board(gya::board const &board,
-                                             i32 depth = -1) {
-        // lmj::print("hi");
-        if (depth == -1) depth = m_depth - 1;
+    [[nodiscard]] eval_result evaluate_board(gya::board const &board) { return evaluate_board(board, m_depth - 1); }
+
+    [[nodiscard]] eval_result evaluate_board(gya::board const &board, i32 depth) {
         if (board.has_won().is_game_over()) {
             if (board.has_won().player_1_won())
-                return board.turn() == gya::board::PLAYER_ONE ? WINNING_MOVE
-                                                              : LOSING_MOVE;
+                return board.turn() == gya::board::PLAYER_ONE ? WINNING_MOVE : LOSING_MOVE;
             if (board.has_won().player_2_won())
-                return board.turn() == gya::board::PLAYER_TWO ? WINNING_MOVE
-                                                              : LOSING_MOVE;
+                return board.turn() == gya::board::PLAYER_TWO ? WINNING_MOVE : LOSING_MOVE;
             if (board.has_won().is_tie()) return TIE_MOVE;
         }
         if (depth == 0) return NEUTRAL_MOVE;
@@ -49,8 +44,7 @@ struct transposition_table_solver {
 
         eval_result best_eval = LOSING_MOVE;
         for (u8 move: board.get_actions()) {
-            eval_result eval =
-                    evaluate_board(board.play_copy(move), depth - 1).incremented();
+            eval_result eval = evaluate_board(board.play_copy(move), depth - 1).incremented();
             if (eval > best_eval) best_eval = eval;
             if (eval.m_depth_until_over == 1 || eval.is_winning()) break;
         }
@@ -60,23 +54,19 @@ struct transposition_table_solver {
     }
 
     [[nodiscard]] u8 operator()(gya::board const &board) {
-        for (auto &[b, eval]: m_ttable)
-            if (gya::compressed_board::decompress(b).size < board.size)
-                m_ttable.erase(b);
+        for (auto &[b, eval] : m_ttable)
+            if (gya::compressed_board::decompress(b).size < board.size) m_ttable.erase(b);
         u8 best_move = gya::BOARD_WIDTH;
         eval_result best_eval = LOSING_MOVE;
         auto actions = board.get_actions();
         // put indices closer to the middle first
         std::sort(std::begin(actions), std::end(actions), [](u8 lhs, u8 rhs) {
-            return std::abs(lhs - gya::BOARD_WIDTH / 2) <
-                   std::abs(rhs - gya::BOARD_WIDTH / 2);
+            return std::abs(lhs - gya::BOARD_WIDTH / 2) < std::abs(rhs - gya::BOARD_WIDTH / 2);
         });
 
         for (u8 move: actions) {
-            const auto eval =
-                    evaluate_board(board.play_copy(move)).incremented();
-            if (best_move == gya::BOARD_WIDTH || eval > best_eval)
-                best_move = move, best_eval = eval;
+            const auto eval = evaluate_board(board.play_copy(move)).incremented();
+            if (best_move == gya::BOARD_WIDTH || eval > best_eval) best_move = move, best_eval = eval;
         }
 
         return best_move;

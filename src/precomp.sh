26b0e1162bad60ad66c6a4dab046bfa0a484de27
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-01-24 12:57:53 +0100

    update compilation scripts

--- src/precomp.sh
+++ src/precomp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ include.hpp -O3 -std=c++20 -fopenmp -march=native -Wpedantic -Wextra -Wall -Wshadow #-Werror
+g++ include.hpp -O3 -O3 -std=c++20 -fopenmp -march=native -isystem ../lib/tiny_dnn/ -Wpedantic -Wextra -Wall -Wshadow #-Werror

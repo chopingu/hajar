c7052a46835c9bdb76583ae97cd2090d9f4b31a8
Adam Amanbaev <adam.amanbaev@gmail.com>
2023-01-26 23:14:48 +0100

    changed nn_test

--- src/nn_test.cpp
+++ src/nn_test.cpp
@@ -17,23 +17,40 @@ using namespace tiny_dnn;
 
 int main() {
     network<sequential> net;
-    net << fully_connected_layer(42, 50)
+    net << fully_connected_layer(43, 200)
         << tanh_layer()
-        << fully_connected_layer(50, 50)
+        << fully_connected_layer(200, 200)
         << tanh_layer()
-        << fully_connected_layer(50, 50)
+        << fully_connected_layer(200, 200)
         << tanh_layer()
-        << fully_connected_layer(50, 7)
-        << tanh_layer();
+        << fully_connected_layer(200, 200)
+        << tanh_layer()
+        << fully_connected_layer(200, 7);
 
+    const f32 learning_rate = 0.01f;
+    const f32 discount_factor = 0.9f;
 
-    const f32 learning_rate = 0.05f;
-    const f32 discount_factor = 0.95f;
+    std::vector<u32> wins;
+    std::vector<u32> draws;
+    std::vector<u32> losses;
+
+    u32 win_cnt;
+    u32 draw_cnt;
+    u32 loss_cnt;
 
     heuristic::n_move_solver s(5);
-    
-    i32 iter = 5000;
+
+    i32 iter = 10000;
     while(iter) {
+        if(!(iter % 100)) {
+            wins.push_back(win_cnt);
+            draws.push_back(draw_cnt);
+            losses.push_back(loss_cnt);
+            win_cnt = 0;
+            draw_cnt = 0;
+            loss_cnt = 0;
+        }
+
         gya::board b;
 
         std::vector<vec_t> input_data;
@@ -51,6 +68,8 @@ int main() {
                 for(u32 j = 0; j < gya::BOARD_WIDTH; j++) 
                     input.push_back(b[j][i]);
 
+            input.push_back(turn);
+
             input_data.push_back(input);
 
             vec_t result = net.predict(input);
@@ -65,7 +84,10 @@ int main() {
                     move = col;
                 }
 
-            if(turn == -1) move = s(b);
+            if(turn == -1) {
+                move = s(b);
+                mx_output = result[move];
+            }
 
             moves.push_back(move);
 
@@ -80,75 +102,32 @@ int main() {
         if(b.has_won().is_tie()) {
             qscore.push_back(0);
             qscore.push_back(0);
+            draw_cnt++;
         }
         else {
             qscore.push_back(-1);
             qscore.push_back(1);
+            if(b.has_won().player_2_won()) 
+                loss_cnt++;
+            else 
+                win_cnt++;
         }
-
-        if(!(iter % 2)) lmj::print(b.to_string());
 
         std::vector<vec_t> q_targets = output_data;
 
         for(u32 i = 0; i < cnt; i++) 
             q_targets[i][moves[i]] += learning_rate * (qscore[i] - q_targets[i][moves[i]]);
 
-        size_t batch_size = 1;
-        size_t epochs = 1;
+        size_t batch_size = 30;
+        size_t epochs = 100;
         adagrad opt;
 
         net.fit<mse>(opt, input_data, q_targets, batch_size, epochs);
 
-        if(!(iter % 2)) lmj::print(iter);
-
         iter--;
     }
 
-    while (true) {
-        gya::board b;
-        i8 turn = 1;
-        // heuristic::transposition_table_solver s{depth};
-        while (!b.has_won().is_game_over()) {
-            // lmj::print((std::string) s.evaluate_board(b), s.m_ttable.size());
-            if (turn == 1) {
-                lmj::print(b.to_string());
-                lmj::timer t{false};
-
-                vec_t input;
-                for(i32 i = gya::BOARD_HEIGHT - 1; i >= 0; i--) 
-                    for(u32 j = 0; j < gya::BOARD_WIDTH; j++) 
-                        input.push_back(b[j][i]);
-
-                vec_t result = net.predict(input);
-
-                u32 move;
-                f32 mx_output = -100;
-                for(u32 col = 0; col < gya::BOARD_WIDTH; col++) 
-                    if(result[col] > mx_output && b[col].height < gya::BOARD_HEIGHT) {
-                        mx_output = result[col];
-                        move = col;
-                    }
-
-                b.play(move);
-
-                printf("%fs\n", t.elapsed());
-            } else {
-                lmj::print(b.to_string());
-                int move;
-                std::cin >> move;
-                b.play(move - 1);
-            }
-            turn *= -1;
-        }
-        lmj::print(b.to_string());
-        // lmj::print((std::string) s.evaluate_board(b));
-
-        if (b.has_won().is_tie()) {
-            std::puts("tie!");
-        } else if (b.has_won().player_2_won()) {
-            std::puts("you won!");
-        } else {
-            std::puts("you lost!");
-        }
-    }
+    wins.push_back(win_cnt);
+    draws.push_back(draw_cnt);
+    losses.push_back(loss_cnt);
 }

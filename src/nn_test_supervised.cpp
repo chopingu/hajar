ef5428cf126faa9373e5d078194ac7350e045f45
Adam Amanbaev <adam.amanbaev@gmail.com>
2023-01-27 23:17:29 +0100

    supervised learning

---
+++ src/nn_test_supervised.cpp
@@ -0,0 +1,137 @@
+#include "include.hpp"
+
+#include "heuristic/brute_force/n_move_solver.hpp"
+#include "heuristic/brute_force/one_move_solver.hpp"
+#include "heuristic/brute_force/transposition_table_solver.hpp"
+#include "heuristic/brute_force/two_move_solver.hpp"
+#include "heuristic/mcts/mcts.hpp"
+#include "heuristic/solver_variations/A.hpp"
+#include "heuristic/solver_variations/Abias.hpp"
+#include "heuristic/solver_variations/simple_n_move_solver.hpp"
+
+#include "../lib/tiny_dnn/tiny_dnn/tiny_dnn.h"
+
+using namespace tiny_dnn;
+
+std::string curr_date;
+
+int main() {
+
+    { // put current time in curr_date variable
+        // https://stackoverflow.com/questions/3673226/how-to-print-time-in-format-2009-08-10-181754-811
+        char buffer[1024]{};
+        time_t timer = time(NULL);
+        strftime(buffer, 26, "%Y-%m-%d-%H-%M-%S", localtime(&timer));
+        curr_date = buffer;
+        lmj::debug(curr_date);
+    }
+
+    size_t in_width = 7;
+    size_t in_height = 6;
+    size_t window_size = 4;
+    size_t in_channels = 1;
+    size_t out_channels = 256;
+    network<sequential> net;
+    net << convolutional_layer(in_width, in_height, window_size, in_channels, out_channels)
+        << tanh_layer(in_width - window_size + 1, in_height - window_size + 1, out_channels)
+        << fully_connected_layer((in_width - window_size + 1) * (in_height - window_size + 1) * out_channels, 64)
+        << tanh_layer()
+        << fully_connected_layer(64, 64)
+        << tanh_layer()
+        << fully_connected_layer(64, 7);
+
+    const f32 discount_factor = 0.9f;
+
+    u32 win_cnt = 0;
+    u32 draw_cnt = 0;
+    u32 loss_cnt = 0;
+
+    std::ofstream win_loss_tie_data(curr_date + ".win_data");
+
+    i32 iter = 10000;
+    lmj::timer timer{false};
+    i32 hours = 5;
+
+    heuristic::n_move_solver s(5);
+
+    while (timer.elapsed() < hours * 60 * 60) {
+        lmj::debug(timer.elapsed());
+        lmj::print(iter);
+        if (!(iter % 1)) {
+            win_loss_tie_data << win_cnt << ' ';
+            win_loss_tie_data << draw_cnt << ' ';
+            win_loss_tie_data << loss_cnt << ' ';
+            win_loss_tie_data << std::endl;
+            win_cnt = 0;
+            draw_cnt = 0;
+            loss_cnt = 0;
+        }
+
+        gya::board b;
+
+        std::vector<vec_t> input_data;
+        std::vector<u8> moves;
+
+        u32 cnt = 0;
+        i8 turn = 1;
+        while (!b.has_won().is_game_over()) {
+            if (!(iter % 2)) lmj::print(b.to_string());
+
+            vec_t input;
+            for (i32 i = gya::BOARD_HEIGHT - 1; i >= 0; i--)
+                for (u32 j = 0; j < gya::BOARD_WIDTH; j++)
+                    input.push_back(b[j][i]);
+
+            input_data.push_back(input);
+
+            u32 move = s(b);
+
+            moves.push_back(move);
+
+            b.play(move, turn);
+
+            turn *= -1;
+            cnt++;
+        }
+
+        f32 qlast = 0, qnlast = 0;
+
+        if (b.has_won().is_tie()) {
+            draw_cnt++;
+            iter--;
+            lmj::debug(input_data.size());
+            continue;
+        } else {
+            qlast = 1, qnlast = -1; 
+            if (b.has_won().player_2_won())
+                loss_cnt++;
+            else
+                win_cnt++;
+        }
+
+        adagrad opt;
+
+        std::reverse(moves.begin(), moves.end());
+        std::reverse(input_data.begin(), input_data.end());
+
+        for(u32 i = 0; i < cnt; i++) {
+            vec_t target = net.predict(input_data[i]);
+            target[moves[i]] = qlast;
+
+            net.fit<mse>(opt, std::vector<vec_t>{input_data[i]}, std::vector<vec_t>{target});
+
+            f32 tmp = qlast;
+            qlast = qnlast;
+            qnlast = tmp * discount_factor;
+        }
+
+        lmj::debug(input_data.size());
+
+        iter--;
+    }
+
+    win_loss_tie_data << win_cnt << ' ';
+    win_loss_tie_data << draw_cnt << ' ';
+    win_loss_tie_data << loss_cnt << ' ';
+    win_loss_tie_data << std::endl;
+}
